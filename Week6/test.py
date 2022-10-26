0782c8b3c3ff01083e0eda424be46baa52dbe6d0
KryterX <49222266+KryterX@users.noreply.github.com>
2022-10-26 17:59:09 -0600

    Update

--- Week6/test.py
+++ Week6/test.py
@@ -1,60 +1,30 @@
+# import pygame module in this program
 import pygame
- 
 pygame.init()
-screen = pygame.display.set_mode((500, 600))
-clock = pygame.time.Clock()
-font = pygame.font.SysFont("Arial", 20)
- 
- 
-class Button:
-    """Create a button, then blit the surface in the while loop"""
- 
-    def __init__(self, text,  pos, font, bg="black", feedback=""):
-        self.x, self.y = pos
-        self.font = pygame.font.SysFont("Arial", font)
-        if feedback == "":
-            self.feedback = "text"
-        else:
-            self.feedback = feedback
-        self.change_text(text, bg)
- 
-    def change_text(self, text, bg="black"):
-        """Change the text whe you click"""
-        self.text = self.font.render(text, 1, pygame.Color("White"))
-        self.size = self.text.get_size()
-        self.surface = pygame.Surface(self.size)
-        self.surface.fill(bg)
-        self.surface.blit(self.text, (0, 0))
-        self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
- 
-    def show(self):
-        screen.blit(button1.surface, (self.x, self.y))
- 
-    def click(self, event):
-        x, y = pygame.mouse.get_pos()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if pygame.mouse.get_pressed()[0]:
-                if self.rect.collidepoint(x, y):
-                    self.change_text(self.feedback, bg="red")
- 
- 
-def mainloop():
-    """ The infinite loop where things happen """
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            button1.click(event)
-        button1.show()
-        clock.tick(30)
-        pygame.display.update()
- 
- 
-button1 = Button(
-    "Click here",
-    (100, 100),
-    font=30,
-    bg="navy",
-    feedback="You clicked me")
- 
-mainloop()
+
+screen=pygame.display.set_mode((100,100))
+
+
+font = pygame.font.SysFont(None, 24)
+img = font.render('hello', True, "BLUE")
+screen.blit(img, (20, 20))
+
+"""img = font.render(sysfont, True, RED)
+rect = img.get_rect()
+pygame.draw.rect(img, BLUE, rect, 1)"""
+running=True    
+    
+    
+while running == True:
+    WinSize=pygame.display.get_window_size()
+    
+    
+        
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running=False
+            
+
+        
+    
+    pygame.display.update()
